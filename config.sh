0c03e657de855279320c710b99a4d79dd77b4e45
andyvip <21304928@qq.com>
2015-08-30 15:32:43 +0800

    bugfix

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 home_vimrc=$HOME/.vimrc
-extra_vimrc="source $PWD/vim/vimrc"
-touch $home_vimrc
-if [ `grep -wc "$extra_vimrc" $home_vimrc` -eq 0 ]; then
-    echo $extra_vimrc >> $home_vimrc
+extra_vimrc=$PWD/.vimrc
+
+echo -e "set runtimepath+=$PWD/vim \nsource $PWD/vim/vimrc" > $extra_vimrc
+if [ `touch $home_vimrc && grep -wc "$extra_vimrc" $home_vimrc` -eq 0 ]; then
+    echo -e "source $extra_vimrc" >> $home_vimrc
 fi
